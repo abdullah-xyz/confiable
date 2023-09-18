42dd822f6ad540dfa4c4dd6d764eacf012d11da3
Abdullah Asad <abdullah@net-c.com>
2023-09-18 21:26:12 +0530

    append to prev commit

--- components/layout/Card.vue
+++ components/layout/Card.vue
@@ -1,10 +1,21 @@
+<script setup lang="ts">
+defineProps({
+    name: String,
+    price: Number,
+    slug: String,
+})
+</script>
+
 <template>
-    <div class="w-[25rem] rounded-md flex flex-col gap-x-8">
-        <img src="/img/tmpCover.jpg" alt="" class="w-full rounded-t-md">
-        <div class="p-4">
-            <h3 class="text-2xl font-bold mb-2">Cousre Name</h3>
-            <p class="text-lg font-medium mb-4">&#8377; 23,424</p>
-            <p>One line description</p>
+    <NuxtLink :to="slug">
+
+        <div class="w-[25rem] rounded-md flex flex-col gap-x-8 border-2">
+            <img src="/img/tmpCover.jpg" alt="" class="w-full rounded-t-md">
+            <div class="p-4">
+                <h3 class="text-2xl font-bold mb-2">{{ name }}</h3>
+                <p class="text-lg font-medium mb-4">&#8377; {{ price }}</p>
+                <p>One line description</p>
+            </div>
         </div>
-    </div>
+    </NuxtLink>
 </template>
---
+++ components/layout/Slider.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import Course from 'types/course'
+
+const props = defineProps<{
+    title: string,
+    courses: Course[]
+}>()
+
+const isDragging = ref(false)
+const cursorPos = ref([0, 0])
+const slider = ref<HTMLDivElement | null>(null)
+
+function onMouseDown(e: MouseEvent) {
+    isDragging.value = true
+    cursorPos.value = [e.pageX, e.pageY]
+
+    window.addEventListener("mousemove", onMouseHold)
+}
+
+function onMouseHold(e: MouseEvent) {
+    console.log(e.movementX, e.movementY)
+    e.preventDefault()
+
+    // requestAnimationFrame(() => {
+    //     const delta = [
+    //         e.pageX - cursorPos.value[0],
+    //         e.pageY - cursorPos.value[1]
+    //     ]
+
+    //     cursorPos.value = [e.pageX, e.pageY]
+
+    slider.value?.scrollBy({
+        left: -e.movementX,
+        top: -e.movementY
+    })
+    // })
+
+}
+
+function onMouseUp(e: MouseEvent) {
+    isDragging.value = false
+    window.removeEventListener('mousemove', onMouseHold)
+}
+
+function slideButton(amt: number) {
+    const width = slider.value?.clientWidth
+
+    if (width && slider.value) {
+        if (amt == 1) {
+            slider.value.scrollLeft += width
+        } else if (amt == -1) {
+            slider.value.scrollLeft -= width
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="w-full">
+        <div class="flex justify-between">
+            <div class="text-3xl font-bold mb-4">{{ title }}</div>
+            <div class="text-3xl">
+                <button @click="slideButton(-1)" class="hover:text-gray-700">
+                    <Icon name="fluent:chevron-left-12-regular" />
+                </button>
+                <button @click="slideButton(1)" class="hover:text-gray-700">
+                    <Icon name="fluent:chevron-right-12-regular" />
+                </button>
+            </div>
+        </div>
+        <div ref="slider" :class="isDragging ? 'cursor-grabbing' : 'snap-proximity snap-x scroll-smooth'"
+            class="flex gap-4 overflow-auto" style="scrollbar-width: none;" @mousedown="onMouseDown" @mouseup="onMouseUp">
+            <LayoutCard v-for="course in courses" class="snap-center" :name=course.title :price=course.price
+                :slug="course.slug" />
+        </div>
+    </div>
+</template>
