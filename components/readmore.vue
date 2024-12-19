23c66e6b578fcbaf2fed3e61628ffe0c351ee320
Abdullah Asad <abdullah@net-c.com>
2024-12-19 16:36:25 +0530

    mentorship added

---
+++ components/readmore.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{ turncated: string }>();
+const expanded = ref(false);
+</script>
+
+<template>
+  <div class="px-4 text-lg">
+    <span v-if="!expanded">{{ turncated.slice(0, 200) }} &nbsp;</span>
+    <span v-else>{{ turncated }} &nbsp;</span>
+    <span class="min-w-6"></span>
+    <span
+      class="underline hover:cursor-pointer"
+      v-if="!expanded"
+      @click="expanded = !expanded"
+      >Read more...</span
+    ><span
+      class="underline hover:cursor-pointer"
+      v-if="expanded"
+      @click="expanded = !expanded"
+      >Read less...</span
+    >
+  </div>
+</template>
--- pages/mentorship.vue
+++ pages/mentorship.vue
@@ -1,3 +1,52 @@
+<script setup lang="ts">
+const text = ref(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum
+      sapien, sagittis in urna et, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum
+      sapien, sagittis in urna et, porttitor malesuada libero. Duis non velit
+      ligula. Sed vitae neque porta, dapibus odio sed, dignissim massa. Integer
+      congue convallis tellus, eu vulputate purus. Donec accumsan, libero nec
+      convallis sollicitudin, elit sem efficitur felis, bibendum venenatis leo
+      leo eu massa. Etiam ullamcorper egestas magna, nec cursus ante. Ut
+      pulvinar et lectus a molestie. Etiam sagittis sit amet est quis ultricies.
+      Donec ut libero quis felis scelerisque ultrices vel et nisl. Cras
+      malesuada urna vel nisi commodo fringilla id at lorem. Curabitur at varius
+      nisi, ut dictum massa. Integer blandit porta tincidunt. Cras convallis
+      vitae erat a dapibus. Fusce est ante, rhoncus vel pretium sit amet,
+      finibus vitae odio. Maecenas congue nunc sit amet risus ultricies
+      sollicitudin. Maecenas rhoncus mollis felis vitae rutrum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum
+      sapien, sagittis in urna et, porttitor malesuada libero. Duis non velit
+      ligula. Sed vitae neque porta, dapibus odio sed, dignissim massa. Integer
+      congue convallis tellus, eu vulputate purus. Donec accumsan, libero nec
+      convallis sollicitudin, elit sem efficitur felis, bibendum venenatis leo
+      leo eu massa. Etiam ullamcorper egestas magna, nec cursus ante. Ut
+      pulvinar et lectus a molestie. Etiam sagittis sit amet est quis ultricies.
+      Donec ut libero quis felis scelerisque ultrices vel et nisl. Cras
+      malesuada urna vel nisi commodo fringilla id at lorem. Curabitur at varius
+      nisi, ut dictum massa. Integer blandit porta tincidunt. Cras convallis
+      vitae erat a dapibus. Fusce est ante, rhoncus vel pretium sit amet,
+      finibus vitae odio. Maecenas congue nunc sit amet risus ultricies
+      sollicitudin. Maecenas rhoncus mollis felis vitae rutrum.`)
+</script>
+
 <template>
-    hey
+  <div class="mx-auto max-w-screen-xl px-4 py-8">
+    <div class="px-3 text-3xl font-medium text-primary">
+      Our Mentorship Programmes
+    </div>
+    <div class="px-4 text-lg">
+      {{text }}
+    </div>
+    <div class="px-3  mt-6 text-2xl font-medium text-primary">
+      Career Mentorship
+    </div>
+    <Readmore :turncated="text"></Readmore>
+    <div class="px-3  mt-6 text-2xl font-medium text-primary">
+      Addiction Recovery Support
+    </div>
+    <Readmore :turncated="text"></Readmore>
+    <div class="px-3  mt-6 text-2xl font-medium text-primary">
+      Decission Making Advice
+    </div>
+    <Readmore :turncated="text"></Readmore>
+  </div>
 </template>
