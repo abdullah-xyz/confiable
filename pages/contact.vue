eec9392239c5a8908bda9f1477149e53086f84d4
Abdullah Asad <abdullah@net-c.com>
2025-01-04 15:13:41 +0530

    added service, contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,47 @@
 <template>
-    hey
+  <div class="mx-auto max-w-screen-xl px-4 py-8">
+    <div class="px-4 py-1 text-lg">
+      <span class="font-medium">Phone:</span> 1234567890
+    </div>
+    <div class="px-4 py-1 text-lg">
+      <span class="font-medium">Email:</span>
+      <a href="mailto:confiablebusiness@gmail.com"
+        >confiablebusiness@gmail.com</a
+      >
+    </div>
+    <div class="px-4 py-1 text-lg">
+      <span class="font-medium">Address:</span>
+      <div class="px-4">123/421, Some Street</div>
+      <div class="px-4">Some Locality</div>
+      <div class="px-4">City, 130031</div>
+    </div>
+    <div class="px-4 py-1 text-lg">
+      Join Us at
+      <div class="px-2 hover:cursor-pointer">
+        <Icon name="skill-icons:instagram" /> Instagram
+      </div>
+      <div class="px-2 hover:cursor-pointer">
+        <Icon name="devicon:linkedin" /> Linkedin
+      </div>
+      <div class="px-2 hover:cursor-pointer">
+        <Icon name="devicon:twitter" /> X (Twitter)
+      </div>
+    </div>
+    <div class="mt-3 rounded-md p-4 shadow-md shadow-secondary max-w-lg text-lg">
+      <div class="flex flex-col gap-2">
+        <div class="flex justify-between">
+          <label for="email">Email:</label>
+          <input name="email" type="email" class="w-72 rounded-md bg-background border border-muted focus:ring-secondary focus:ring-1 focus:outline-none text-base" />
+        </div>
+        <div class="flex justify-between">
+          <label for="name">Name:</label
+          ><input name="name" type="text" class="w-72 rounded-md bg-background border border-muted focus:ring-secondary focus:ring-1 focus:outline-none text-base" />
+        </div>
+        <div class="flex justify-between">
+          <label for="feedback">Feedback:</label
+          ><textarea name="feedback" rows="4" class="w-72 rounded-md bg-background border border-muted focus:ring-secondary focus:ring-1 focus:outline-none text-base" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- pages/services.vue
+++ pages/services.vue
@@ -1,18 +1,73 @@
+<script setup lang="ts">
+const serviceGroups = [
+  {
+    title: "Business Service",
+    content: [
+      { title: "Business Ideas and Platform", icon: "logo-c.png" },
+      { title: "Business Investment Guidance", icon: "logo-c.png" },
+      { title: "Training and Development Programs", icon: "logo-c.png" },
+      {
+        title: "Business Analysis and Optimization Report",
+        icon: "logo-c.png",
+      },
+      {
+        title: "Logistics, Network and Brand Building",
+        icon: "logo-c.png",
+      },
+    ],
+  },
+  {
+    title: "Financial Service",
+    content: [
+      { title: "Business Ideas and Platform", icon: "logo-c.png" },
+      { title: "Business Investment Guidance", icon: "logo-c.png" },
+      { title: "Training and Development Programs", icon: "logo-c.png" },
+      {
+        title: "Business Analysis and Optimization Report",
+        icon: "logo-c.png",
+      },
+      {
+        title: "Logistics, Network and Brand Building",
+        icon: "logo-c.png",
+      },
+    ],
+  },
+];
+</script>
+
 <template>
-    <div class="bg-background">
-        <div class="max-w-screen-2xl mx-auto py-3">
-            <h3 class="text-3xl font-medium text-primary">
-                Business Services   
-            </h3>
-            <p class="text-lg py-2">Confiable is a company dedicated to helping young entrepreneurs and students transform their
-ideas into million-dollar ventures. We offer a vast range of services, including over 500 business
-ideas across various sectors such as trading, manufacturing, and online business. Our expertise
-extends to guiding investments, from basic to substantial ventures, and providing tailored
-training for businesses, shops, and startups.</p>
-<p class="text-lg py-2">We specialize in creating detailed business reports, analyzing operations, and addressing any
-operational gaps through expert training and strategic management. Additionally, Confiable
-supports businesses with logistics, networking, and branding to enhance their market presence
-and drive success.</p>
-        </div>
+  <div class="mx-auto max-w-screen-xl px-4 py-8">
+    <div class="px-3 text-3xl font-medium text-primary">
+      Our Offered Services
     </div>
+    <div class="px-4 text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ipsum
+      sapien, sagittis in urna et, porttitor malesuada libero. Duis non velit
+      ligula. Sed vitae neque porta, dapibus odio sed, dignissim massa. Integer
+      congue convallis tellus, eu vulputate purus. Donec accumsan, libero nec
+      convallis sollicitudin, elit sem efficitur felis, bibendum venenatis leo
+      leo eu massa. Etiam ullamcorper egestas magna, nec cursus ante. Ut
+      pulvinar et lectus a molestie. Etiam sagittis sit amet est quis ultricies.
+      Donec ut libero quis felis scelerisque ultrices vel et nisl. Cras
+      malesuada urna vel nisi commodo fringilla id at lorem. Curabitur at varius
+      nisi, ut dictum massa. Integer blandit porta tincidunt. Cras convallis
+      vitae erat a dapibus. Fusce est ante, rhoncus vel pretium sit amet,
+      finibus vitae odio. Maecenas congue nunc sit amet risus ultricies
+      sollicitudin. Maecenas rhoncus mollis felis vitae rutrum.
+    </div>
+    <Accordian :items="serviceGroups">
+      <template #content="{ content }">
+        <div v-for="service in content">
+          <NuxtLink to="/lecture">
+            <div
+              class="my-2 flex items-center rounded-md border border-transparent px-4 py-2 text-lg hover:underline hover:shadow hover:shadow-secondary"
+            >
+        <NuxtImg class="mr-2 size-5" :src="'/img/' + service.icon" />
+        <span class="font-medium">{{ service.title }}</span>
+            </div>
+          </NuxtLink>
+        </div>
+      </template>
+    </Accordian>
+  </div>
 </template>
